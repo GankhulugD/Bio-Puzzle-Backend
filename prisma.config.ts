48029dddb74e49bca8203fa84115ddd1127747e8
GankhulugD <gankhulug12345@gmail.com>
2026-05-08 17:19:21 +0800

    -

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: "tsx src/scripts/seed.ts",
+  },
 });
